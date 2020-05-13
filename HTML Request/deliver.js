8aece4d52915978f5ec463a8ef87b59ce766372d
Cole Alexander <cbielefeld@cnm.edu>
2020-05-13 09:00:08 -0600

    snap challenge 5/13/2020

---
+++ HTML Request/deliver.js
@@ -0,0 +1,23 @@
+const getBtn = document.getElementById('get-btn')
+const postBtn = document.getElementById('post-btn')
+
+ const getData = () => {
+  axios.get('https://reques.in/api/users').then(response => {
+    console.log(response);
+  });
+ };
+
+const sendData = () => {
+  axios.post('https://reques.in/api/register', {
+    email: 'eve.holt@reqres.in'
+    password: 'pistol'
+  }).then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.log(err, err,response);
+  });
+};
+
+getBtn.addEventListener('click', getData);
+postBtn.addEventListener('click', sendData);
