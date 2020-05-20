a0f947aad15e58137971318ce6cc4dd3aa3f34f3
Cole Alexander <cbielefeld@cnm.edu>
2020-05-20 08:55:14 -0600

    Reduce snap challenge 5/20/2020

---
+++ javascript-reduce/reduce-string.js
@@ -0,0 +1,5 @@
+["Deflector", "power", "at", "maximum.", "Energy", "discharge", "in", "six", "seconds.", "Warp", "reactor", "core", "primary", "coolant", "failure.", "Fluctuate", "phaser", "resonance", "frequencies.", "Resistance", "is", "futile.", "Recommend", "we", "adjust", "shield", "harmonics", "to", "the", "upper", "EM", "band", "when", "proceeding.", "These", "appear", "to", "be", "some", "kind", "of", "power-wave-guide", "conduits", "which", "allow", "them", "to", "work", "collectively", "as", "they", "perform", "ship", "functions.", "Increase", "deflector", "modulation", "to", "upper", "frequency", "band."]
+
+var result = authors.map(function( author ) {
+  return author.name;
+}).join('Deflector, power, at, maximum.');
